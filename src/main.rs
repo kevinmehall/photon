e18f8c2f750d6e9d948b278014d0468efbb5226f
Kevin Mehall <km@kevinmehall.net>
2022-05-30 11:13:27 -0700

    Add CLI query subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,73 @@
-use std::{sync::Arc, net::SocketAddr, convert::Infallible};
 use clap::Parser;
+use photon::Config;
+use std::{convert::Infallible, net::SocketAddr, sync::Arc, io};
 use tokio::sync::RwLock;
-use photon::{Config};
 
 mod server;
 
 /// Log analysis and visualization tool
 #[derive(Parser, Debug)]
 #[clap(version, about)]
-struct Args {
-    /// Directory containing configuration files
-    #[clap(short, long)]
-    config_dir: std::path::PathBuf,
+enum Args {
+    Serve {
+        /// Directory containing configuration files
+        #[clap(short, long)]
+        config_dir: std::path::PathBuf,
 
-    #[clap(short, long, default_value_t = SocketAddr::from(([127, 0, 0, 1], 3333)))]
-    listen: SocketAddr,
+        #[clap(short, long, default_value_t = SocketAddr::from(([127, 0, 0, 1], 3333)))]
+        listen: SocketAddr,
+    },
+    Query {
+        /// Directory containing configuration files
+        #[clap(short, long)]
+        config_dir: std::path::PathBuf,
+
+        #[clap(short, long)]
+        dataset: String,
+
+        #[clap(short, long)]
+        query: String,
+    },
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
 
-    let config = Arc::new(RwLock::new(Config::load(args.config_dir).unwrap()));
+    match args {
+        Args::Serve { config_dir, listen } => {
+            let config = Arc::new(RwLock::new(Config::load(config_dir).unwrap()));
+            let service = hyper::service::service_fn(move |req| {
+                let config = config.clone();
+                async move {
+                    Result::<_, Infallible>::Ok(match server::handle_request(&config, req).await {
+                        Ok(res) => res,
+                        Err(e) => e.into_response(),
+                    })
+                }
+            });
 
-    let service = hyper::service::service_fn(move |req| {
-        let config = config.clone();
-        async move {
-            Result::<_, Infallible>::Ok(match server::handle_request(&config, req).await {
-                Ok(res) => res,
-                Err(e) => e.into_response(),
-            })
+            let make_service = hyper::service::make_service_fn(move |_conn| {
+                std::future::ready(Ok::<_, Infallible>(service.clone()))
+            });
+
+            hyper::Server::bind(&listen)
+                .serve(make_service)
+                .await
+                .unwrap();
         }
-    });
+        Args::Query {
+            config_dir,
+            dataset,
+            query,
+        } => {
+            let config = Config::load(config_dir).unwrap();
+            let dataset = config.dataset(&dataset).expect("dataset does not exist").expect("config error");
 
-    let make_service = hyper::service::make_service_fn(move |_conn| {
-        std::future::ready(Ok::<_, Infallible>(service.clone()))
-    });
+            let query = serde_json::from_str(&query).expect("failed to parse query");
+            let results = dataset.query(&query).expect("failed to run query");
 
-    hyper::Server::bind(&args.listen)
-        .serve(make_service)
-        .await
-        .unwrap();
+            serde_json::to_writer(io::stdout().lock(), &results).expect("failed to write output");
+        }
+    }
 }
